f84450aa5f1de23dac4a8db7d15de78a4be85a21
Guochen Zhou <42365862+viogentseries@users.noreply.github.com>
2021-11-11 18:33:55 +0800

    Update test_prs.py

--- optimizers/prs/test_prs.py
+++ optimizers/prs/test_prs.py
@@ -2,7 +2,7 @@ import unittest
 import time
 import numpy as np
 
-from base_functions import ellipsoid
+from base_functions import ellipsoid, rosenbrock, rastrign
 from prs import PRS as Solver
 
 
@@ -10,7 +10,7 @@ class TestPRS(unittest.TestCase):
     def test_optimize(self):
         start_run = time.time()
         ndim_problem = 1000
-        for f in [ellipsoid]:
+        for f in [ellipsoid, rosenbrock, rastrign]:
             print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
             problem = {'fitness_function': f,
                        'ndim_problem': ndim_problem,
