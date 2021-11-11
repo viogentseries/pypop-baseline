32fbb56a95f187d7d4f79326d4059c0b1c883ac5
Guochen Zhou <42365862+viogentseries@users.noreply.github.com>
2021-11-11 19:39:59 +0800

    Update test_rhc_from_pybrain.py

--- optimizers/rhc/test_rhc_from_pybrain.py
+++ optimizers/rhc/test_rhc_from_pybrain.py
@@ -5,7 +5,7 @@ from base_functions import ellipsoid, rosenbrock, rastrigin
 from pybrain.optimization import HillClimber as HC
 for f in [ellipsoid, rosenbrock, rastrigin]:
     print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
-    solver = HC(ellipsoid, 4 * np.ones((1000,)), minimize=True, maxEvaluations=2e6, verbose=True)
+    solver = HC(f, 4 * np.ones((1000,)), minimize=True, maxEvaluations=2e6, verbose=True)
     start_time = time.time()
     solver.learn()
     print("Runtime: {:7.5e}".format(time.time() - start_time))
