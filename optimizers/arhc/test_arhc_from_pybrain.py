a59d7c7a7a73f6b9611b177fb1bdbab8f8506069
Guochen Zhou <42365862+viogentseries@users.noreply.github.com>
2021-11-13 09:57:41 +0800

    Update test_arhc_from_pybrain.py

--- optimizers/arhc/test_arhc_from_pybrain.py
+++ optimizers/arhc/test_arhc_from_pybrain.py
@@ -1,10 +1,11 @@
 import time
 import numpy as np
 
-from base_functions import ellipsoid
+from base_functions import ellipsoid,rosenbrock, rastrigin
 from pybrain.optimization import StochasticHillClimber as SHC
-
-solver = SHC(ellipsoid, 4 * np.ones((1000,)), minimize=True, maxEvaluations=2e6, verbose=True)
-start_time = time.time()
-solver.learn()
-print("Runtime: {:7.5e}".format(time.time() - start_time))
+for f in [ellipsoid, rosenbrock, rastrigin]:
+    print('*' * 7 + ' ' + f.__name__ + ' ' + '*' * 7)
+    solver = SHC(f, 4 * np.ones((1000,)), minimize=True, maxEvaluations=2e6, verbose=True)
+    start_time = time.time()
+    solver.learn()
+    print("Runtime: {:7.5e}".format(time.time() - start_time))
